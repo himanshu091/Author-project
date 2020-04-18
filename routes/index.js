0bc18270a79f2921110bab67774c332520498d2f
himanshu091 <himanshu091rawat@gmail.com>
2020-04-18 17:28:03 +0530

    Initial routing for authors page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
+// All Authors
+
 router.get('/',(req,res)=>{
     res.render('index');
-})
-
-
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 const expresslayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true,useUnifiedTopology: true})
@@ -26,9 +28,7 @@ app.set('views',__dirname + '/views');
 app.set('layout','layouts/layout');
 app.use(expresslayouts);
 app.use(express.static('public'));
-
- 
-
-app.use('/',indexRouter);     
-
+app.use(bodyParser.urlencoded({limit :'10mb',extended: false }));
+app.use('/',indexRouter);
+app.use('/authors',authorRouter);     
 app.listen(process.env.PORT || 3000);
